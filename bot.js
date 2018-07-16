ba4e26d77dfcab98216bbe032101f374a4ab1cf3
ReachYourDream <radityopradana@gmail.com>
2018-07-16 20:28:53 +0700

    commit 2

---
+++ bot.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const line = require('@line/bot-sdk');
+const express = require('express');
+const https = require('https');
+const http = require('http');
+var timeout = require('connect-timeout');
+var echo = { type: 'text', text: 'Untuk sementara fitur yang bisa digunakan hanya: \n\
+  dosen(spasi)nama dosen\n\
+  Contoh: Dosen Rudi' };
+  var echo_carousel = {"type": "template",
+  "altText": "this is a carousel template",
+  "template": {
+    "type": "carousel",
+    "actions": [],
+    "columns": [
+      {
+        "thumbnailImageUrl": "SPECIFY_YOUR_IMAGE_URL",
+        "title": "Title",
+        "text": "Text",
+        "actions": [
+          {
+            "type": "message",
+            "label": "Action 1",
+            "text": "Action 1"
+          },
+          {
+            "type": "message",
+            "label": "Action 2",
+            "text": "Action 2"
+          }
+        ]
+      },
+      {
+        "thumbnailImageUrl": "SPECIFY_YOUR_IMAGE_URL",
+        "title": "Title",
+        "text": "Text",
+        "actions": [
+          {
+            "type": "message",
+            "label": "Action 1",
+            "text": "Action 1"
+          },
+          {
+            "type": "message",
+            "label": "Action 2",
+            "text": "Action 2"
+          }
+        ]
+      }
+    ]
+  }}
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+const client = new line.Client(config);
+const app = express();
+
+app.post('/callback', line.middleware(config), (req, res) => {
+
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+function handleEvent(event) {
+	if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+  const insert_text = String(event.message.text);
+  echo.text = 'Coba test';
+  return client.replyMessage(event.replyToken,echo);
+}
