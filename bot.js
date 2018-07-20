39f20902294bea132e91a0d327bdda0f665b5ff7
ReachYourDream <radityopradana@gmail.com>
2018-07-20 23:57:54 +0700

    return gambar

--- bot.js
+++ bot.js
@@ -23,6 +23,13 @@ app.post('/callback', line.middleware(config), (req, res) => {
 
 function handleEvent(event) {
   console.log(event);
+  if(event.message.type =='image'){
+    var id_gambar = event.message.id;
+    var gambar = {type:'image', id=''};
+    gambar.id= id_gambar;
+    console.log('gambar: ' + $gambar);
+    return client.replyMessage(event.replyToken,gambar);
+  }
 	if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
@@ -41,3 +48,21 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
+
+// { type: 'message',
+// 2018-07-16T14:18:15.611959+00:00 app[web.1]:   replyToken: 'bcace659aaf3411ea394051378170b63',
+// 2018-07-16T14:18:15.611962+00:00 app[web.1]:   source: { userId: 'U0f70c027f512a5252dea0b9498e775cd', type: 'user' },
+// 2018-07-16T14:18:15.611963+00:00 app[web.1]:   timestamp: 1531750695138,
+// 2018-07-16T14:18:15.611965+00:00 app[web.1]:   message:
+// 2018-07-16T14:18:15.611967+00:00 app[web.1]:    { type: 'location',
+// 2018-07-16T14:18:15.611968+00:00 app[web.1]:      id: '8274111258384',
+// 2018-07-16T14:18:15.611970+00:00 app[web.1]:      address: 'Ruang A1 No.19 Ketawanggede, Kecamatan Lowokwaru, Kota Malang, Jawa Timur 65145',
+// 2018-07-16T14:18:15.611972+00:00 app[web.1]:      latitude: -7.954879789956453,
+// 2018-07-16T14:18:15.611994+00:00 app[web.1]:      longitude: 112.61452244422676 } }
+
+
+// { type: 'message',
+// 2018-07-16T14:20:37.698623+00:00 app[web.1]:   replyToken: 'a842937c6f2d4bcabfd948acda5c9017',
+// 2018-07-16T14:20:37.698625+00:00 app[web.1]:   source: { userId: 'Ud5572a7dae7213d18788691958d98fa4', type: 'user' },
+// 2018-07-16T14:20:37.698627+00:00 app[web.1]:   timestamp: 1531750836737,
+// 2018-07-16T14:20:37.698628+00:00 app[web.1]:   message: { type: 'image', id: '8274124138927' } }
